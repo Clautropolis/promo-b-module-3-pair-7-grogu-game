bfac4932f415f902f9db6e484618615f669730b1
Claudia <claurodher2@gmail.com>
2025-01-22 11:06:01 +0100

    migration

---
+++ src/components/App.jsx
@@ -0,0 +1,53 @@
+
+import '../styles/App.scss'
+
+
+function App() {
+
+  return (
+      <>
+      <header>
+      <h1>¡Cuidado con Grogu!</h1>
+      </header>
+      <main className="page">
+        <section className="board">
+        <div className="cell"><div className="grogu">👣</div></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        </section>
+
+        <section>
+        <button className="dice">Lanzar Dado</button>
+        <div className="game-status">En curso</div>
+        </section>
+
+        <section className="goods-container">
+        <div className="goods-item">🍪</div>
+        <div className="goods-item">🍪</div>
+        <div className="goods-item">🍪</div>
+        </section>
+        <section className="goods-container">
+        <div className="goods-item">🥚</div>
+        <div className="goods-item">🥚</div>
+        <div className="goods-item">🥚</div>
+        </section>
+        <section className="goods-container">
+        <div className="goods-item">🐸</div>
+        <div className="goods-item">🐸</div>
+        <div className="goods-item">🐸</div>
+        </section>
+        <section>
+        <button className="restart-button">Reiniciar Juego</button>
+        </section>
+    </main>
+      
+  </>
+    
+  );
+}
+
+export default App
